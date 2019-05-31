5663e71a951b83928e30b8937f5a9170564237a9
zhaojiwei <zhaoqw@rd.netease.com>
2019-05-31 13:53:35 +0800

    将

--- src/Adiff/Task/ArrayTask.ts
+++ src/Adiff/Task/ArrayTask.ts
@@ -1,8 +1,7 @@
 import { Task, ITaskProps } from "./Task";
 import { ObjectTask } from "./ObjectTask";
 import { TextTask, PrimitiveTask } from ".";
-import { Interface } from "readline";
-
+import lcs from './lcs'
 export interface IArrayTaskProps extends ITaskProps {
   type: string
   itemIdentifier?: string
@@ -35,8 +34,8 @@ export class ArrayTask extends Task {
   }
 
   public handle = () => {
-    let commonHead = 0;
-    let commonTail = 0;
+    let unMatchHead = 0;
+    let unMatchTail = 0;
     let index;
     let index1;
     let index2;
@@ -45,59 +44,139 @@ export class ArrayTask extends Task {
     let len1 = array1.length;
     let len2 = array2.length;
 
-    // separate common head
+    // find head of uncommon part
     while (
-      commonHead < len1 &&
-      commonHead < len2 &&
-      this.isItemsMatch(array1[commonHead], array2[commonHead])
+      unMatchHead < len1 &&
+      unMatchHead < len2 &&
+      this.isItemsMatch(array1[unMatchHead], array2[unMatchHead])
     ) {
-      index = commonHead
+      index = unMatchHead
       this.assignNewTask({
         left: array1[index],
         right: array2[index],
         type: index
       })
-      commonHead++
+      unMatchHead++
     }
 
-    // separate common tail
+    // find tail of uncommon part
     while (
-      commonHead + commonTail < len1 &&
-      commonHead + commonTail < len2 &&
+      unMatchHead + unMatchTail < len1 &&
+      unMatchHead + unMatchTail < len2 &&
       this.isItemsMatch(
-        array1[len1 - 1 - commonTail],
-        array2[len2 - 1 - commonTail]
+        array1[len1 - 1 - unMatchTail],
+        array2[len2 - 1 - unMatchTail]
       )
     ) {
-      index1 = len1 - 1 - commonTail
-      index2 = len2 - 1 - commonTail;
+      index1 = len1 - 1 - unMatchTail
+      index2 = len2 - 1 - unMatchTail;
       this.assignNewTask({
         left: array1[index1],
         right: array2[index2],
         type: index2
       })
-      commonHead++
+      unMatchTail++
     }
 
+    // case: arrays are identical
+    if (unMatchHead + unMatchTail === len1 && len1 === len2) {
+      return
+    }
+
+    // case: only add items
+    if (unMatchHead + unMatchTail === len1) {
+      for (let index = unMatchHead; index < len2 - unMatchTail; index++) {
+        this.assignNewTask({
+          left: undefined,
+          right: array2[index],
+          type: '' + index
+        })
+      }
+      return
+    }
+
+    // case: only remove items
+    if (unMatchHead + unMatchTail === len2) {
+      for (let index = unMatchHead; index < len1 - unMatchTail; index++) {
+        this.assignNewTask({
+          left: array1[index],
+          right: undefined,
+          type: '' + index
+        })
+      }
+      return
+    }
+
+    // case: add, remove, and edit
+    const unMatchPart1 = array1.slice(unMatchHead, len1 - unMatchTail)
+    const unMatchPart2 = array2.slice(unMatchHead, len2 - unMatchTail)
+
+    let seq = this.getLCS(unMatchPart1, unMatchPart2);
 
 
 
 
+  }
 
+  private lengthMatrix = (array1: any[], array2: any[]) => {
+    const len1 = array1.length;
+    const len2 = array2.length;
 
+    // initialize empty matrix of (len1 + 1) * (len2 + 1)
+    const matrix = Array.from(
+      { length: len1 + 1 },
+      () => Array.from({ length: len2 + 1 }, () => 0)
+    )
 
+    for (let i = 1; i <= len1; i++) {
+      for (let j = 1; j <= len2; j++) {
+        const temp = this.isItemsMatch(array1[i - 1], array2[j - 1])
+          ? matrix[i][j] = matrix[i - 1][j - 1] + 1
+          : Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+        matrix[i][j] = temp
+      }
+    }
 
+    return matrix
+  }
 
+  private backtrack = (matrix, array1, array2, index1, index2) => {
+    if (index1 === 0 || index2 === 0) {
+      return {
+        sequence: [],
+        indices1: [],
+        indices2: [],
+      };
+    }
 
+    if (this.isItemsMatch(array1[index1], array2[index2])) {
+      const sub = this.backtrack(
+        matrix,
+        array1,
+        array2,
+        index1 - 1,
+        index2 - 1,
+      );
+      sub.sequence.push(array1[index1 - 1]);
+      sub.indices1.push(index1 - 1);
+      sub.indices2.push(index2 - 1);
+      return sub;
+    }
 
+    return matrix[index1][index2 - 1] > matrix[index1 - 1][index2]
+      ? this.backtrack(matrix, array1, array2, index1, index2 - 1)
+      : this.backtrack(matrix, array1, array2, index1 - 1, index2);
+  }
 
-
-
-
-
-
-
-
+  private getLCS = (array1, array2) => {
+    const matrix = this.lengthMatrix(array1, array2)
+    return this.backtrack(
+      matrix,
+      array1,
+      array2,
+      array1.length,
+      array2.length
+    )
   }
 
   private assignNewTask = (
